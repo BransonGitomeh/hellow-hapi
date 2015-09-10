cf28be32117b2477206782a2a51d89d16490a5ff
BransonGitomeh <sirbranson@outlook.com>
2015-09-11 00:49:10 +0300

    added 'relative paths'

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ var Hapi = require('hapi');
 //lib for logging server events
 var Good = require('good');
 
+//some weird shit they call relative files
+
+//TODO explain wth this is when i get it
+var Path = require('path');
+
 //make a new server object
-var server = new Hapi.Server();
+var server = new Hapi.Server({
+  connections:{
+    routes:{
+      files:{
+        relativeTo:Path.join(__dirname,'public')
+      }
+    }
+  }
+});
 
 
 //this must be there or it wiil throw a stupid error
@@ -40,7 +53,6 @@ server.route({
   method:'GET',
   path:'/{name}/{age}',
   handler:function(request,reply){
-
     //since the good console logger is now part of the server you can use it
     server.log('info', 'server running at ; ' + server.info.uri)
     reply('Heeey, ' + encodeURIComponent(request.params.name) + ' ' + encodeURIComponent(request.params.age) + ' im happy')
@@ -48,9 +60,20 @@ server.route({
 })
 
 //endpoint for random picture from fs
+//old method
 server.route({
   method:'GET',
   path:'/randomPicture',
+  handler:function(request,reply){
+    reply.file('/home/bransongk/Downloads/husky.jpg')
+  }
+})
+
+//endpoint for random picture from fs
+//new method now with relative paths
+server.route({
+  method:'GET',
+  path:'/husky.jpg',
   handler:function(request,reply){
     reply.file('/home/bransongk/Downloads/husky.jpg')
   }
